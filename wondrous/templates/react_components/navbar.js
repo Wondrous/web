efb3d36e148ab45b1de3f73a2a7635bc11546bb5
David Siah <dsiah@villanova.edu>
2015-02-07 15:08:38 -0500

    react navbar almost complete

--- wondrous/templates/react_components/navbar.js
+++ wondrous/templates/react_components/navbar.js
@@ -1,7 +1,51 @@
-var Hello = React.createClass({
-    render: function() {
-        return <div>Hello {this.props.name}</div>;
-    }
+// My first React components :D
+var SearchBox = React.createClass({
+	render: function () {
+		return (
+			<form method="GET" action="/search/" style={{"display": "inline-block"}}>
+        <div className="container">
+          <input type="text" id="query" className="banner-input" 
+          	placeholder="Search for people and #tags" name="q" 
+          	data-provide="typeahead" autoComplete="off" />
+        </div>
+    	</form>);
+	}
 });
- 
-React.render(<Hello name="World" />, document.body);
+
+var SettingsGear = React.createClass({
+	render: function () {
+		return (
+			<span className="banner-more-options">
+        <span className="banner-options-icon">C</span>
+    	</span>);
+	}
+});
+
+var ProfileLink = React.createClass({
+	render: function () {
+		return (
+			<a id="linkToProfile" href={"/" + this.props.name} 
+			className="general-text banner-user-name">
+        <img className="banner-user-img round-3" 
+        src="/static/pictures/defaults/p.default-profile-picture.jpg" />
+        {this.props.name}
+    	</a>)
+	}
+});
+
+var Navbar = React.createClass({
+	render: function () {
+		return (
+			<div id="topBanner" className="top-banner">
+				<a href="/" style={{"color": "rgb(235, 235, 235)"}}>
+					<img src="/static/pictures/p.icon_50x50.png" className="banner-logo" />
+				</a>
+				<SearchBox />
+				<SettingsGear />
+				{/* This name is static and must change to fetch username or ID !!*/}
+				<ProfileLink name="dsiah" /> 
+					</div>);
+	}
+});
+
+React.render(<Navbar />, document.body);
