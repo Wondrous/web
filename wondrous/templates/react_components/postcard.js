210bcb906a1b93de69ea6afc90a01786baa367da
David Siah <dsiah@villanova.edu>
2015-02-08 21:22:10 -0500

    added superflous semicolon

--- wondrous/templates/react_components/postcard.js
+++ wondrous/templates/react_components/postcard.js
@@ -1,5 +1,26 @@
+var item_owner = {
+	"profile_picture": 1,
+	"username": "boobyBelch",
+	"person": {"name" : "Bob Belcher"}
+}
+
+var UserTitle = React.createClass({
+	render: function () {
+		var user = this.props.name;
+		return (
+			<div>
+				<img className="post-thumb round-50" src={user.profile_picture} />
+				<span className="post-identifier ellipsis-overflow">
+				    <a href="/static ">{user.person.name}</a>
+				</span>
+			</div>);
+	}
+});
+
 var Postcard = React.createClass({
 	render: function () {
-		return (<div className="post-body round-2">Card + + + Card</div>)
+		return (<div className="post-body round-2">
+			<UserTitle name={item_owner} />
+			Card</div>);
 	}
 });
