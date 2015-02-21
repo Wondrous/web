18c6ae3671d084507dd932cdbc9911d2d28a3db6
jrz1 <john@wondrous.co>
2015-02-21 09:00:48 -0500

    John loves spacing. Must add whitespace to appease the space gods.

--- wondrous/models/feed.py
+++ wondrous/models/feed.py
@@ -16,25 +16,27 @@ from sqlalchemy import (
 
 from sqlalchemy.orm import relationship
 
-from wondrous.models import (
-    Base,
-    DBSession,
-)
+from wondrous.models import Base
 
 from wondrous.models.modelmixins import BaseMixin
 
-class FeedPostLink(Base,BaseMixin):
+class FeedPostLink(Base, BaseMixin):
+
     """
         This is the link for the many to many relationship
         between an object and its post(s)
     """
+
     feed_id = Column(BigInteger, ForeignKey('feed.id'))
     post_id = Column(BigInteger, ForeignKey('post.id'))
 
+
 class Feed(Base, BaseMixin):
+
     """
         This defines the feed table which is responsible for loading
-        the primary feed
+        the primary (Majority) feed
     """
+
     user_id = Column(BigInteger, ForeignKey('user.id'), nullable=False)
     feed_post_links = relationship("FeedPostLink", backref="feed")
