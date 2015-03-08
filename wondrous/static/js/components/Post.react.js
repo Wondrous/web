e1e43255fb7b3a5063cd421f2ea62e9d46e6d58d
jrz1 <john@wondrous.co>
2015-03-08 12:31:33 -0400

    Made the like icon reflect if you've liked the post

--- wondrous/static/js/components/Post.react.js
+++ wondrous/static/js/components/Post.react.js
@@ -53,7 +53,7 @@ var Photo = React.createClass({
                                 </span>
 
                                 <span className="post-micro-data-super-analytics-item">
-                                    <img src="/static/pictures/icons/like/heart_gray_shadow.svg" className="post-general-icon" style={{height: 18, width: 18, top: 5, marginRight: 2}} />
+                                    <img src={this.props.data.liked ? "/static/pictures/icons/like/heart_red.svg" : "/static/pictures/icons/like/heart_gray_shadow.svg"} className="post-general-icon" style={{height: 18, width: 18, top: 5, marginRight: 2}} />
                                     ?
                                 </span>
                             </div>
