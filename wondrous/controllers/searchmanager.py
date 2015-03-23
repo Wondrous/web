23f05184fd9e7e93bda9ee6fc853f69a8497aa93
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2015-03-23 10:56:06 -0400

    sqlalchemy op

--- wondrous/controllers/searchmanager.py
+++ wondrous/controllers/searchmanager.py
@@ -28,15 +28,14 @@ class SearchManager:
     def user_search_json(user,search,page):
         search = search.lower()
         users = DBSession.query(User).\
-                join(Vote, \
-                    (User.is_private==False)|((Vote.user_id==user.id)&(Vote.subject_id==User.id)&((Vote.status==Vote.FOLLOWED) | (Vote.status==Vote.TOPFRIEND))\
-                 )).\
+                join(Vote,(Vote.subject_id==User.id)&((Vote.status==Vote.FOLLOWED) | (Vote.status==Vote.TOPFRIEND))).\
                 filter(\
                     (func.lower(User.username).ilike("%{0}%".format(search)))|\
                     (func.lower(User.description).ilike("%{0}%".format(search)))|\
                     (func.lower(User.name).ilike("%{0}%".format(search)))|\
                     (func.lower(User.ascii_name).ilike("%{0}%".format(search)))\
                 ).\
+                filter((User.is_private==False)|(Vote.user_id==user.id)).\
                 distinct().offset(page*15).limit(15).all()
 
         retval = []
@@ -53,13 +52,11 @@ class SearchManager:
         ret = DBSession.query(Post,Vote).\
             join(Object,Post.object_id==Object.id).\
             join(User, User.id==Post.user_id).\
-            join(v1, (User.is_private==False) |\
-                ( (v1.user_id==user.id) \
-                    & ((v1.subject_id == Post.user_id)) &\
-                     ((v1.status==Vote.FOLLOWED) | (v1.status==Vote.TOPFRIEND)) )).\
             outerjoin(Vote, (Vote.subject_id==Post.id)&(Vote.user_id==user.id)&(Vote.status==Vote.LIKED)).\
+            outerjoin(v1, (((Post.owner_id==v1.subject_id )|(Post.user_id==v1.subject_id))&((v1.status==6) or (v1.status==7)))).\
             filter(or_(Object.subject.ilike("%{q}%".format(q=search)),Object.text.ilike("%{q}%".format(q=search)))).\
             filter(Post.set_to_delete==None).\
+            filter((User.is_private==False)|(v1.user_id==user.id)).\
             offset(page*15).limit(15).all()
         retval = []
         for post, vote in ret:
@@ -96,12 +93,10 @@ class SearchManager:
         ret = DBSession.query(Post,Vote).\
             join(Tag, Tag.post_id==Post.id).\
             join(User, User.id==Post.user_id).\
-            join(v1, (User.is_private==False) |\
-                ( (v1.user_id==user.id) \
-                    & ((v1.subject_id == Post.user_id)) &\
-                     ((v1.status==Vote.FOLLOWED) | (v1.status==Vote.TOPFRIEND)) )).\
             outerjoin(Vote, (Vote.subject_id==Post.id)&(Vote.user_id==user.id)&(Vote.status==Vote.LIKED)).\
+            outerjoin(v1, (((Post.owner_id==v1.subject_id )|(Post.user_id==v1.subject_id))&((v1.status==6) or (v1.status==7)))).\
             filter(Post.set_to_delete==None).\
+            filter((User.is_private==False)|(v1.user_id==user.id)).\
             filter(func.lower(Tag.tag_name).in_(tags)).\
             offset(page*15).limit(15).all()
 
