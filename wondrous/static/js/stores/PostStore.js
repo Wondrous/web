187d1068845d8f67cbf23754390cec92d873f642
jrz1 <john@wondrous.co>
2015-03-24 00:11:31 -0400

    Styling the edit btn and the post-more-options

--- wondrous/static/js/stores/PostStore.js
+++ wondrous/static/js/stores/PostStore.js
@@ -7,18 +7,18 @@ var defaultUser = {username:''};
 var PostStore = Reflux.createStore({
     listenables: WondrousActions,
 
-    init:function(){
+    init: function() {
         this.unloadUser();
         this.listenTo(UserStore,"onUserChange");
     },
 
-    unloadUser: function(){
-        this.post = {subject:'',text:'',id:-1};
+    unloadUser: function() {
+        this.post = { subject: '',text: '',id: -1 };
         this.commentPage = 0;
         this.userPage = 0;
 
-        this.comments = new FeedSet(null,true);
-        this.likedUsers = new FeedSet(null,true,false);
+        this.comments = new FeedSet(null, true);
+        this.likedUsers = new FeedSet(null, true, false);
         this.postError = null;
 
         this.commentPaging = false;
@@ -30,99 +30,108 @@ var PostStore = Reflux.createStore({
         this.loading = false;
 
         this.postLink = null;
+
+        this.moreOptions = false;
     },
 
     closeCardModal: function(){
         this.postLink = null;
+        this.moreOptions = false;
     },
 
-    togglePostLink: function(){
-        if(this.postLink==null){
+    togglePostLink: function() {
+        if (this.postLink === null) {
             this.postLink = "https://"+window.location.host+'/post/'+this.post.id;
-        }else{
+        } else {
             this.postLink=null;
         }
         this.trigger();
     },
 
-    newPostLoad: function(post_id){
-        this.unloadUser();
-        if (typeof post_id !=='undefined'&&!this.loading){
-            this.loading = true;
+    toggleMoreOptions: function() {
+        this.moreOptions = !this.moreOptions;
+        this.trigger();
+    },
 
+    newPostLoad: function(post_id) {
+        this.unloadUser();
+        if (typeof post_id !== 'undefined' && !this.loading){
+            this.loading = true;
             this.post.id = post_id;
             WondrousActions.openCardModal();
             WondrousActions.loadPost(post_id);
             this.loadMoreComments();
-
         }
     },
 
-    loadMoreComments: function(){
-        if (typeof this.post.id !=='undefined' &&this.post.id!=-1&& !this.commentPaging && !this.doneCommentPaging){
+    loadMoreComments: function() {
+        if (typeof this.post.id !=='undefined' && this.post.id != -1 && !this.commentPaging && !this.doneCommentPaging) {
             this.commentPaging = true;
-            WondrousActions.loadComments(this.post.id,this.commentPage);
-            console.log("loading comments",this.post.id,this.commentPage);
+            WondrousActions.loadComments(this.post.id, this.commentPage);
+            console.log("loading comments", this.post.id, this.commentPage);
             this.incrementCommentPage();
         }
     },
 
-    loadMoreLikedUsers: function(){
-        if (typeof this.post.id !=='undefined' &&this.post.id!=-1&& !this.likedUserPaging && !this.doneLikedUserPaging){
+    loadMoreLikedUsers: function() {
+        if (typeof this.post.id !=='undefined' && this.post.id != -1 && !this.likedUserPaging && !this.doneLikedUserPaging) {
             this.likedUserPaging = true;
-            WondrousActions.loadLikedUsers(this.post.id,this.userPage);
+            WondrousActions.loadLikedUsers(this.post.id, this.userPage);
             this.userPage++;
         }
     },
 
-    loadCommentsError: function(err){
+    loadCommentsError: function(err) {
         this.commentPaging = false;
         this.trigger();
     },
 
-    commentError: function(err){
+    commentError: function(err) {
 
     },
 
-    incrementCommentPage: function(){
+    incrementCommentPage: function() {
         this.commentPage++;
     },
 
 
-    onUserChange: function(userData){
+    onUserChange: function(userData) {
 
     },
-    updatePostOnModal: function(post){
-        if(typeof post!=='undefined'){
+
+    updatePostOnModal: function(post) {
+        if (typeof post !== 'undefined') {
             this.post = post;
         }
-        this.trigger({post:this.post,comments:this.comments.sortedSet});
+        this.trigger({ post: this.post, comments: this.comments.sortedSet });
     },
-    updatePost: function(postData){
+
+    updatePost: function(postData) {
         this.post = postData;
         WondrousActions.updatePostOnWall();
         WondrousActions.updatePostOnFeed();
         this.loading = true;
-        this.trigger({post:this.post,comments:this.comments.sortedSet});
+        this.trigger({ post: this.post, comments: this.comments.sortedSet });
     },
 
-    loadPostError: function(err){
+    loadPostError: function(err) {
         this.comments.reset();
         this.loading = false;
         this.commentPaging = false;
         this.postError = err.error;
-        this.trigger({post:this.post,comments:this.comments.sortedSet});
+        this.trigger({ post: this.post, comments: this.comments.sortedSet });
     },
 
-    addToComments: function(comment){
+    addToComments: function(comment) {
         if(this.post.id == comment.post_id) {
             this.comments.push(comment);
             this.post.comment_count++;
             WondrousActions.updatePostOnWall();
             WondrousActions.updatePostOnFeed();
-            this.trigger({post:this.post,comments:this.comments.sortedSet});
+            this.trigger({ post: this.post, comments: this.comments.sortedSet });
         }
     },
+
     updateLikedUsers: function(users){
         this.likedUserPaging = false;
         if (users.length < 15) {
@@ -141,18 +150,18 @@ var PostStore = Reflux.createStore({
             this.doneCommentPaging = true;
         }
 
-        comments.map(function(comment,index){
+        comments.map(function(comment,index) {
             this.push(comment);
         }, this.comments);
-        this.post.comment_count=this.comments.sortedSet.length;
+        this.post.comment_count = this.comments.sortedSet.length;
         WondrousActions.updatePostOnWall();
         WondrousActions.updatePostOnFeed();
-        this.trigger({post: this.post, comments: this.comments.sortedSet});
+        this.trigger({ post: this.post, comments: this.comments.sortedSet });
     },
 
-    removeFromComment: function(comment_id){
+    removeFromComment: function(comment_id) {
         this.comments.delete(comment_id);
-        this.trigger({post:this.post,comments:this.comments.sortedSet});
+        this.trigger({ post: this.post, comments: this.comments.sortedSet });
     }
 
 });
