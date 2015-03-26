b405966672335a6125041e0cd2c4dca921e81c90
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2015-03-26 15:46:23 -0400

    more scripting fixed

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -e
 
-BASEDIR=`dirname $0`
+BASEDIR=`dirname $0`/..
 
-PYRAMIDENV = `dirname $0`/../pyramidenv
-
-if [ ! -d "$PYRAMIDENV" ]; then
-    virtualenv -q $PYRAMIDENV --no-site-packages
+if [ ! -d "$BASEDIR/pyramidenv" ]; then
+    virtualenv -q $BASEDIR/pyramidenv --no-site-packages
     echo "Virtualenv created."
 fi
 
-if [ ! -f "$PYRAMIDENV/updated" -o $BASEDIR/requirements.txt -nt $PYRAMIDENV/updated ]; then
-    pip install -r $BASEDIR/requirements.txt -e $PYRAMIDENV
-    touch $PYRAMIDENV/updated
+if [ ! -f "$BASEDIR/pyramidenv/updated" -o $BASEDIR/requirements.pip -nt $BASEDIR/pyramidenv/updated ]; then
+    pip install -r $BASEDIR/requirements.pip -e $BASEDIR/pyramidenv
+    touch $BASEDIR/pyramidenv/updated
     echo "Requirements installed."
 fi
