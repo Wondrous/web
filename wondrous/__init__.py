8b4770d11d167ea6e5d40a5b7233245d1ead7be6
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2015-03-26 12:36:26 -0400

    deployment requirement cleanup

--- wondrous/__init__.py
+++ wondrous/__init__.py
@@ -68,33 +68,33 @@ def main_app(global_config, **settings):
 
     return config.make_wsgi_app()
 
-
-def admin_app(global_config, **settings):
-
-    """ This function returns a Pyramid WSGI application. """
-
-    settings['app_root'] = abspath(dirname(dirname(__file__)))
-    initialize_sql(settings)
-
-    config = Configurator(settings=settings)
-    config = build_admin_routes(config)  # URL map on admin_routes.py
-
-    authn_policy = AuthTktAuthenticationPolicy(ADMIN_AUTH_TKN, hashalg='sha512')
-    authz_policy = ACLAuthorizationPolicy()
-
-    config.set_authentication_policy(authn_policy)  # Needed for global user object
-    config.set_authorization_policy(authz_policy)   # Needed becasue I have an Authentication policy
-
-    config.add_request_method(AuthHelper.get_admin, 'admin', reify=True)  # GLOBAL ADMIN OBJECT
-
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.include('pyramid_jinja2')
-    config.add_jinja2_search_path("wondrous:templates")
-
-    config.scan(ignore=[
-        "wondrous.views.main",
-        "wondrous.views.ajax_main",
-        "wondrous.views.api_views"
-    ])
-
-    return config.make_wsgi_app()
+#
+# def admin_app(global_config, **settings):
+#
+#     """ This function returns a Pyramid WSGI application. """
+#
+#     settings['app_root'] = abspath(dirname(dirname(__file__)))
+#     initialize_sql(settings)
+#
+#     config = Configurator(settings=settings)
+#     config = build_admin_routes(config)  # URL map on admin_routes.py
+#
+#     authn_policy = AuthTktAuthenticationPolicy(ADMIN_AUTH_TKN, hashalg='sha512')
+#     authz_policy = ACLAuthorizationPolicy()
+#
+#     config.set_authentication_policy(authn_policy)  # Needed for global user object
+#     config.set_authorization_policy(authz_policy)   # Needed becasue I have an Authentication policy
+#
+#     config.add_request_method(AuthHelper.get_admin, 'admin', reify=True)  # GLOBAL ADMIN OBJECT
+#
+#     config.add_static_view('static', 'static', cache_max_age=3600)
+#     config.include('pyramid_jinja2')
+#     config.add_jinja2_search_path("wondrous:templates")
+#
+#     config.scan(ignore=[
+#         "wondrous.views.main",
+#         "wondrous.views.ajax_main",
+#         "wondrous.views.api_views"
+#     ])
+#
+#     return config.make_wsgi_app()
