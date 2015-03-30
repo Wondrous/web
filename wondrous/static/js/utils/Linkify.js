b577f0813dddb14cbf17be323666a95d455d7efc
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2015-03-30 15:14:24 -0400

    edited production + fixed linkify asterisk

--- wondrous/static/js/utils/Linkify.js
+++ wondrous/static/js/utils/Linkify.js
@@ -20,11 +20,13 @@ var Linkify = function(rawText, hashtagOverrideClass) {
             var href = null;
             if (tk.indexOf('@') > -1) {
                 var temp = tk.replace('@','')
+                var temp = tk.replace('*','')
                 href = '/'+temp;
 
                 isHashtag = false;
             } else if (tk.indexOf('#') > -1) {
                 var temp = tk.replace('#','')
+                var temp = tk.replace('*','')
                 href = '/tags/'+temp;
                 isHashtag = true;
             }
