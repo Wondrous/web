4ff4fd72df175039e411f3fe1817270302a6c481
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2015-04-01 19:05:11 -0400

    automated front end build + upload

--- wondrous/static/build.sh
+++ wondrous/static/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [ ! -d dist ]; then
+	mkdir dist;
+fi
 npm run build
 cp css/style.css dist/style.css
 cp js/bundle.min.js dist/bundle.min.js
+cp -av pictures/ dist/pictures
+cp -av css/fonts dist/fonts
 echo "built and copied to dist"
+aws s3 sync dist s3://wondrousstatic 
+
