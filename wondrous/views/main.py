97b5e7b6f149beda652b000e835d4b8f13c261ae
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2015-04-01 22:09:47 -0400

    meta tagging

--- wondrous/views/main.py
+++ wondrous/views/main.py
@@ -77,10 +77,10 @@ class AuthHandler(BaseHandler):
 
 
 class IndexHandler(BaseHandler):
-    @view_config(renderer='/index-html.jinja2', route_name='index_handler')
-    @view_config(renderer='/index-html.jinja2', route_name='index_handler1')
-    @view_config(renderer='/index-html.jinja2', route_name='index_handler2')
-    @view_config(renderer='/index-html.jinja2', route_name='index_handler3')
+    @view_config(renderer='/index.jinja2', route_name='index_handler')
+    @view_config(renderer='/index.jinja2', route_name='index_handler1')
+    @view_config(renderer='/index.jinja2', route_name='index_handler2')
+    @view_config(renderer='/index.jinja2', route_name='index_handler3')
     def index(self):
 
         """
