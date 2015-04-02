403fe6603326158319d59b6d56be0c4699f44780
jrz1 <john@wondrous.co>
2015-04-02 02:16:39 -0400

    Fixed login internal server error -- needs to be deployed

--- wondrous/views/main.py
+++ wondrous/views/main.py
@@ -89,42 +89,44 @@ class IndexHandler(BaseHandler):
         """
 
         base_url = 'http://mojorankdev.s3.amazonaws.com/'
-
-        retval = {'page_title':None, 'page_url': None, 'page_content': None, 'page_image': None, 'social_page':True}
-        if 'cat' in self.request.matchdict.keys() and 'post_id' in self.request.matchdict.keys():
+        req_md   = self.request.matchdict
+        retval   = {'page_title': None, 'page_url': None, 'page_content': None, 'page_image': None, 'social_page': True}
+        
+        if 'cat' in req_md.keys() and 'post_id' in req_md.keys():
             # see if it is post
-            retval['social_page'] = self.request.matchdict['cat'].lower()=='post'
+            retval['social_page'] = req_md['cat'].lower()=='post'
             try:
-                post_id = int(self.request.matchdict['post_id'])
+                post_id = int(req_md['post_id'])
                 post_json = PostManager.get_by_id_json(post_id)
                 if 'error' in post_json.keys():
                     retval['social_page'] = False
                 else:
-                    retval['page_title'] = post_json['subject']
-                    retval['page_url'] = "https://wondrous.co/post/{0}".format(post_json['id'])
+                    retval['page_title']   = post_json['subject']
+                    retval['page_url']     = "https://wondrous.co/post/{0}".format(post_json['id'])
                     retval['page_content'] = post_json['text']
-                    retval['page_image'] = base_url + post_json['ouuid']+'-med'
+                    retval['page_image']   = base_url + post_json['ouuid']+'-med'
 
             except Exception, e:
                 retval['social_page'] = False
 
-        elif 'cat' in self.request.matchdict.keys() and self.request.matchdict['cat']!='favicon.ico' and self.request.matchdict['cat']!='signup':
+        elif ('cat' in req_md.keys() and
+              req_md['cat'] not in ['favicon.ico','signup','login']):
             # is an user
-            username = self.request.matchdict['cat']
+            username  = req_md['cat']
             user_json = AccountManager.get_json_by_username(username=username)
 
             if 'error' in user_json.keys():
                 retval['social_page'] = False
             elif user_json['is_private']:
-                retval['page_title'] = "{0} @{1}".format(user_json['name'],user_json['username'])
-                retval['page_url'] = "https://wondrous.co/{0}".format(user_json['username'])
+                retval['page_title']   = "{0} @{1}".format(user_json['name'],user_json['username'])
+                retval['page_url']     = "https://wondrous.co/{0}".format(user_json['username'])
                 retval['page_content'] = "This profile is private"
-                retval['page_image'] = "https://s3-us-west-2.amazonaws.com/wondrousstatic/pictures/defaults/p.default-profile-picture.jpg"
+                retval['page_image']   = "https://s3-us-west-2.amazonaws.com/wondrousstatic/pictures/defaults/p.default-profile-picture.jpg"
             else:
-                retval['page_title'] = "{0} @{1}".format(user_json['name'],user_json['username'])
-                retval['page_url'] = "https://wondrous.co/{0}".format(user_json['username'])
+                retval['page_title']   = "{0} @{1}".format(user_json['name'],user_json['username'])
+                retval['page_url']     = "https://wondrous.co/{0}".format(user_json['username'])
                 retval['page_content'] = user_json['description']
-                retval['page_image'] = base_url + user_json['ouuid']+'-dim150x150'
+                retval['page_image']   = base_url + user_json['ouuid']+'-dim150x150'
 
         else:
             retval['social_page'] = False
