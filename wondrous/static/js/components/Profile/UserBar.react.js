8c0e3f8243c456a2332c120fae6a69771e68ec64
jrz1 <john@wondrous.co>
2015-04-05 13:33:52 -0400

    Added in the profile picture prompt banner

--- wondrous/static/js/components/Profile/UserBar.react.js
+++ wondrous/static/js/components/Profile/UserBar.react.js
@@ -4,6 +4,7 @@ var ProfileStore = require('../../stores/ProfileStore');
 var UserStore = require('../../stores/UserStore');
 var ProfileBarBadge = require('./ProfileBarBadge.react');
 var InfluencerBadge = require('./InfluencerBadge.react');
+var InformationBanner = require('../InformationBanner.react');
 var linkify = require('../../utils/Linkify');
 
 var URLGenerator = require('../../utils/URLGenerator');
@@ -99,69 +100,80 @@ var UserBar = React.createClass({
         }
 
         return (
-            <div className="profile-header">
-                <div className="profile-photo-wrapper">
-                    <img className="profile-photo round-50" style={is_me ? {cursor: 'pointer'} : {}} onClick={this.handleClick} src={img_src} />
-                </div>
+            <span>
+                {is_me && !ouuid ?
+                    <InformationBanner
+                        bannerType="alert"
+                        actionType="uploadProfilePicture" 
+                        canExit="false"
+                        text="We encourage you to upload a profile picture – it will make your profile look 100x better!"
+                        styles={{ margin: 0, cursor: "pointer" }}
+                        />
+                    : null}
+                <div className="profile-header">
+                    <div className="profile-photo-wrapper">
+                        <img className="profile-photo round-50" style={is_me ? {cursor: 'pointer'} : {}} onClick={this.handleClick} src={img_src} />
+                    </div>
 
-                <div className="profile-header-content">
+                    <div className="profile-header-content">
 
-                    {is_influencer ?
-                        <InfluencerBadge size="large" />
-                        : null}
+                        {is_influencer ?
+                            <InfluencerBadge size="large" />
+                            : null}
 
-                    <div className="profile-name">{this.state.data.name}</div>
-                    <div className="profile-username">@{this.state.data.username}</div>
+                        <div className="profile-name">{this.state.data.name}</div>
+                        <div className="profile-username">@{this.state.data.username}</div>
 
-                    {this.state.editing ?
-                        <form onSubmit={this.onSubmitDescription}>
-                            <textarea className="profile-about-textarea profile-about--font" ref="descriptionBox" placeholder="Tell us something about yourself!">
-                                {this.state.data.description.length > 0 ? this.state.data.description : null}
-                            </textarea>
-                            <div><input className="post-comment-btn" type="submit" value="Save" /></div>
-                        </form>
-                        :
-                        <span>
-                            {this.state.data.description.length > 0 ?
-                                <div className="profile-about profile-about--font">
-                                    {linkify(this.state.data.description, "hashtagify--medium hashtagify--ghost")}
-                                    {is_me ? <span className="profile-about--edit" onClick={this.onEdit}>Edit</span> : null}
-                                </div>
-                                :
-                                <span>
-                                    {is_me ? <div className="profile-about--edit profile-about--edit-prompt" onClick={this.onEdit}>Add a description about yourself</div> : null}
-                                </span>
-                            }
-                        </span>
-                    }
-                </div>
-                <hr className="profile-hr" />
-                <ul className="profile-header-nav">
-                    <ProfileBarBadge to={"wall"} name={"posts"} number={this.state.data.post_count} username={this.state.data.username} />
-                    <ProfileBarBadge to={"followers"} name={"followers"} number={this.state.data.follower_count-1} username={this.state.data.username} />
-                    <ProfileBarBadge to={"following"} name={"following"} number={this.state.data.following_count-1} username={this.state.data.username} />
-                    <ProfileBarBadge to={"wall"} name={"influence"} number={wondrousScore} username={this.state.data.username} />
-
-                    {!is_me ?
-                        <div>
-                            <li className={classes} onClick={this.handleFollow}>
-                                <div className="profile-header-nav-title" style={{ color: "rgb(140,140,140)" }} >{btnTitle}</div>
-                                {!this.am_following ?
+                        {this.state.editing ?
+                            <form onSubmit={this.onSubmitDescription}>
+                                <textarea className="profile-about-textarea profile-about--font" ref="descriptionBox" placeholder="Tell us something about yourself!">
+                                    {this.state.data.description.length > 0 ? this.state.data.description : null}
+                                </textarea>
+                                <div><input className="post-comment-btn" type="submit" value="Save" /></div>
+                            </form>
+                            :
+                            <span>
+                                {this.state.data.description.length > 0 ?
+                                    <div className="profile-about profile-about--font">
+                                        {linkify(this.state.data.description, "hashtagify--medium hashtagify--ghost")}
+                                        {is_me ? <span className="profile-about--edit" onClick={this.onEdit}>Edit</span> : null}
+                                    </div>
+                                    :
                                     <span>
-                                        <span className="wondrous-connect-btn-plus">+</span>
-                                        <img style={{ display: "none"}} className="wondrous-connect-btn-checkmark" src="https://s3-us-west-2.amazonaws.com/wondrousstatic/pictures/icons/checkmark/checkmark-1.png?v=1" />
-                                    </span>
-                                        :
-                                    <span>
-                                        <span style={{ display: "none"}} className="wondrous-connect-btn-plus">+</span>
-                                        <img className="wondrous-connect-btn-checkmark" src="https://s3-us-west-2.amazonaws.com/wondrousstatic/pictures/icons/checkmark/checkmark-1.png?v=1" />
+                                        {is_me ? <div className="profile-about--edit profile-about--edit-prompt" onClick={this.onEdit}>Add a description about yourself</div> : null}
                                     </span>
                                 }
-                            </li>
-                        </div>
-                        : null}
-                </ul>
-            </div>
+                            </span>
+                        }
+                    </div>
+                    <hr className="profile-hr" />
+                    <ul className="profile-header-nav">
+                        <ProfileBarBadge to={"wall"} name={"posts"} number={this.state.data.post_count} username={this.state.data.username} />
+                        <ProfileBarBadge to={"followers"} name={"followers"} number={this.state.data.follower_count-1} username={this.state.data.username} />
+                        <ProfileBarBadge to={"following"} name={"following"} number={this.state.data.following_count-1} username={this.state.data.username} />
+                        <ProfileBarBadge to={"wall"} name={"influence"} number={wondrousScore} username={this.state.data.username} />
+
+                        {!is_me ?
+                            <div>
+                                <li className={classes} onClick={this.handleFollow}>
+                                    <div className="profile-header-nav-title" style={{ color: "rgb(140,140,140)" }} >{btnTitle}</div>
+                                    {!this.am_following ?
+                                        <span>
+                                            <span className="wondrous-connect-btn-plus">+</span>
+                                            <img style={{ display: "none"}} className="wondrous-connect-btn-checkmark" src="https://s3-us-west-2.amazonaws.com/wondrousstatic/pictures/icons/checkmark/checkmark-1.png?v=1" />
+                                        </span>
+                                            :
+                                        <span>
+                                            <span style={{ display: "none"}} className="wondrous-connect-btn-plus">+</span>
+                                            <img className="wondrous-connect-btn-checkmark" src="https://s3-us-west-2.amazonaws.com/wondrousstatic/pictures/icons/checkmark/checkmark-1.png?v=1" />
+                                        </span>
+                                    }
+                                </li>
+                            </div>
+                            : null}
+                    </ul>
+                </div>
+            </span>
         );
     }
 });
