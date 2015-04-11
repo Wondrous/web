4dc7c02551292993eea542947c60e20efb28c1d8
jrz1 <john@wondrous.co>
2015-04-11 16:47:38 -0400

    Back to proper social buttons

--- wondrous/static/js/components/Modals/PostFooter.react.js
+++ wondrous/static/js/components/Modals/PostFooter.react.js
@@ -116,16 +116,17 @@ var PostFooter = React.createClass({
                     <img src="https://s3-us-west-2.amazonaws.com/wondrousstatic/pictures/icons/link/link.png" className="post-delete-icon" />
                 </span>
 
-                <div>
-                    <a href="javascript:" onClick={function(evt) { return window.open(twitterLink) }} target="_blank" >Twitter</a>
-                </div>
-                <div>
-                    <a href="javascript:" onClick={function(evt) { return window.open(facebookLink) }} target="_blank" >Facebook</a>
-                </div>
-
                 {PostStore.postLink != null ?
                     <input className="post-share-link-input round-2" type='text' value={PostStore.postLink} readOnly/>
                     : {}}
+
+                <a href="javascript:" onClick={function(evt) { return window.open(facebookLink) }} target="_blank" >
+                    <img className="post-footer-btn round-50" style={{ position: "relative", top: 7 }} src="https://s3-us-west-2.amazonaws.com/wondrousstatic/pictures/icons/social/social-facebook.png" title="Share this post to Facebook" />
+                </a>
+
+                <a href="javascript:" onClick={function(evt) { return window.open(twitterLink) }} target="_blank" >
+                    <img className="post-footer-btn round-50" style={{ position: "relative", top: 7 }} src="https://s3-us-west-2.amazonaws.com/wondrousstatic/pictures/icons/social/social-twitter.png" title="Share this post to Twitter" />
+                </a>
 
                 <span onClick={WondrousActions.toggleMoreOptions} className="post-footer-btn post-options-btn round-50 nh" style={{ paddingTop: "3px" }} title="See more options">
                     +
