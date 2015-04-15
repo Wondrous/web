37ef543599328ec824285424f4fce64f26a31e42
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2015-04-15 11:49:04 -0400

    fixes

--- wondrous/controllers/feedmanager.py
+++ wondrous/controllers/feedmanager.py
@@ -48,7 +48,7 @@ class FeedManager(BaseManager):
             if not post.is_hidden and post.is_active and not post.set_to_delete:
                 post_dict = post.json()
                 post_dict.update({'liked': vote!=None})
-                
+
                 # Add in the wondrous score
                 score = AccountManager.calculate_wondrous_score(post.user)
                 if score:
@@ -114,10 +114,8 @@ class FeedManager(BaseManager):
 
         # If the profile_user is public, we dont need to check for relationship, else do
         # If we are logged in and the profile_user happens to be private, we have to check for relationship
-        if (not profile_user.is_private and not profile_user.is_banned and profile_user.is_active) or \
-            (profile_user.is_private and not profile_user.is_banned and profile_user.is_active and profile_user):
+        if profile_user and not profile_user.is_banned and profile_user.is_active:
             v1 = aliased(Vote)
-
             # based on
             # SELECT DISTINCT post.id AS post_id, v1.id AS vote_id
             # FROM post
@@ -137,7 +135,7 @@ class FeedManager(BaseManager):
             retval = DBSession.query(Post,Vote).\
                 join(User, (User.id==Post.user_id)|(User.id==Post.owner_id)).\
                 outerjoin(Vote, (Vote.subject_id==Post.id)&(Vote.user_id==my_user_id)&(Vote.status==Vote.LIKED)).\
-                outerjoin(v1, (((Post.owner_id==v1.subject_id )|(Post.user_id==v1.subject_id))&((v1.status==6) or (v1.status==7)))).\
+                outerjoin(v1, (((Post.owner_id==v1.subject_id )|(Post.user_id==v1.subject_id))&((v1.status==Vote.FOLLOWED)|(v1.status==Vote.TOPFRIEND)))).\
                 filter(Post.user_id==profile_user.id).\
                 filter((User.is_private==False)|(v1.user_id==my_user_id)).\
                 filter(Post.set_to_delete==None).\
